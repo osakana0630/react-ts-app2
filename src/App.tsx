5048eaf0a5fd41102599a272255b885542457a18
osakana0630 <osakana0630@gmail.com>
2021-03-09 02:01:27 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {UserCard} from "../src/components/UserCard"
+import axios from "axios";
+import {User} from "./types/api/user";
+import {UserProfile} from "./types/userProfile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const onClickFetchData = () => {
+        setLoading(true);
+        setError(false);
+        axios.get<Array<User>>("https://jsonplaceholder.typicode.com/usersj")
+            .then(res => {
+                const data = res.data.map((user) => ({
+                    id: user.id,
+                    name: `${user.name}(${user.username})`,
+                    email: user.email,
+                    address: `${user.address.city}${user.address.suite}${user.address.street}`
+                }));
+                setUserProfiles(data);
+            })
+            .catch(e => setError(true))
+            .finally(() => setLoading(false));
+    };
+    return (
+        <div className="App">
+            <button onClick={onClickFetchData}>データを取得</button>
+            <br/>
+            {error ? (
+                <p style={{color: "red"}}>データの取得に失敗しました。</p>
+            ) : loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    {userProfiles.map((user) => (
+                        <UserCard key={user.id} user={user}/>
+                    ))}
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
